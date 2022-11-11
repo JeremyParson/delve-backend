4e035ea2ebb93c146a9503f06407195395b3b9fe
Jeremy Parson <jsbparson@gmail.com>
2022-11-11 14:46:34 -0500

    debug

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,17 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const NODE_ENV = process.env.NODE_ENV;
-let url = "";
-if (NODE_ENV == "testing") {
-  url = process.env.TESTING_DATABASE_URL
-} else {
-  url = process.env.DATABASE_URL
-}
-console.log("connecting to", url)
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url
+      url: "postgresql://postgres:postgres@localhost:5432/delve?schema=public"
     }
   }
 });
